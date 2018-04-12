b8eb013c06693c2489d7489f1f39ffdfe6aad38c
bartholomeo <bialcus@gmail.com>
2018-04-12 20:30:09 +0100

    Added settings file.

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import sys
 import pygame
+from setting import Settings
 
 def run_game():
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("DP Invasion")
     bg_color = (230, 230, 230)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
         pygame.display.flip()
 
 run_game()
