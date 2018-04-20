1b86426c0fd0d214633664652449c1a377851afc
bartholomew <bialcus@gmail.com>
2018-04-20 21:14:26 +0100

    Added firing bullets functionality.

--- game_functions.py
+++ game_functions.py
@@ -1,22 +1,27 @@
 import sys
 import pygame
+from bullet import Bullet
 
-def check_events(hero):
+def check_events(hero, ai_settings, screen, bullets):
     """Respond to keypresses and mouse events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, hero)
+            check_keydown_events(event, hero, ai_settings, screen, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, hero)
 
-def check_keydown_events(event, hero):
+def check_keydown_events(event, hero, ai_settings, screen, bullets):
     """Respond to keypresses."""
     if event.key == pygame.K_RIGHT:
         hero.moving_right = True
     elif event.key == pygame.K_LEFT:
         hero.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        #Create a new bullet and add it to the bullets group.
+        new_bullet = Bullet(ai_settings, screen, hero)
+        bullets.add(new_bullet)
 
 def check_keyup_events(event, hero):
     """Respond to keypresses."""
@@ -26,11 +31,15 @@ def check_keyup_events(event, hero):
         hero.moving_left = False
 
 
-def update_screen(ai_settings, screen, hero):
+def update_screen(ai_settings, screen, hero, bullets):
     """Update images on the screen and flip to the new screen."""
     #Redraw the screen during each pass through the loop.
     screen.fill(ai_settings.bg_color)
     hero.blitme()
+
+    #Redraw all bullets behind hero and enemies.
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
 
     #Make the most recently drawn screen visible.
     pygame.display.flip()
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from pygame.sprite import Group
 from settings import Settings
 from hero import Hero
 import game_functions as gf
@@ -10,13 +11,18 @@ def run_game():
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("DP Invasion")
+    #Make a group to store bullets in.
+    bullets = Group()
+
+    #Create a hero
     hero = Hero(screen, ai_settings)
     bg_color = (230, 230, 230)
 
     #main game loop
     while True:
-        gf.check_events(hero)
+        gf.check_events(hero, ai_settings, screen, bullets)
         hero.update()
-        gf.update_screen(ai_settings, screen, hero)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, hero, bullets)
 
 run_game()
