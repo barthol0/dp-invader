f692607b2c68c876cf8e3760506fbfdbd7743c3c
bartholomew <bialcus@gmail.com>
2018-04-22 00:20:55 +0100

    Refactor.

--- game_functions.py
+++ game_functions.py
@@ -19,10 +19,7 @@ def check_keydown_events(event, hero, ai_settings, screen, bullets):
     elif event.key == pygame.K_LEFT:
         hero.moving_left = True
     elif event.key == pygame.K_SPACE:
-        #Create a new bullet and add it to the bullets group.
-        if len(bullets)< ai_settings.bullets_allowed:
-            new_bullet = Bullet(ai_settings, screen, hero)
-            bullets.add(new_bullet)
+        fire_bullet(ai_settings, screen, hero, bullets)
 
 def check_keyup_events(event, hero):
     """Respond to keypresses."""
@@ -44,3 +41,22 @@ def update_screen(ai_settings, screen, hero, bullets):
 
     #Make the most recently drawn screen visible.
     pygame.display.flip()
+
+def update_bullets(bullets):
+    """Update position of bullets and get rid of old bullets."""
+    #Update bullet positions.
+    bullets.update()
+    
+    #Get rid of bullets that have disappeared.
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+    print(len(bullets))
+
+def fire_bullet(ai_settings, screen, hero, bullets):
+    """Fire a bullet if limit not reached yet."""
+    #Create a new bullet and add it to the bullets group.
+    if len(bullets)< ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, hero)
+        bullets.add(new_bullet)
+    
